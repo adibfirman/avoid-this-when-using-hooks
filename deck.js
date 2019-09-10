5ee3866db2c56b8faa748dcb940d91b319c4bc7d
Adib Firman <dib.firman@gmail.com>
2019-09-10 10:44:47 +0700

    updated intro

--- deck.js
+++ deck.js
@@ -1,5 +1,6 @@
 export { nightOwlFull as theme } from "code-surfer";
 
-import { slides as introduction } from "./pages/introduction.mdx";
+import { slides as Introduction } from "./pages/introduction.mdx";
+import { slides as PitfallOne } from "./pages/pitfall-1/slides.mdx";
 
-export const slides = [...introduction];
+export const slides = [...Introduction, ...PitfallOne];
