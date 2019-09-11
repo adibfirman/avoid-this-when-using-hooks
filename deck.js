443b0ad0827c05af8702c39123ef13b56bb301a9
Adib Firman <dib.firman@gmail.com>
2019-09-11 10:03:58 +0700

    change command to NPM

--- deck.js
+++
@@ -1,6 +0,0 @@
-export { nightOwlFull as theme } from "code-surfer";
-
-import { slides as Introduction } from "./pages/introduction.mdx";
-import { slides as PitfallOne } from "./pages/pitfall-1/slides.mdx";
-
-export const slides = [...Introduction, ...PitfallOne];
